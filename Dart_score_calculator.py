20f62b0a3892f538470eedaae612418fa4c2e61d
KostasEreksonas <k.ereksonas@gmail.com>
2021-06-17 14:21:28 +0300

    Added a function to calculate average score

--- Dart_score_calculator.py
+++ Dart_score_calculator.py
@@ -11,6 +11,12 @@ leg_1 = 0
 leg_2 = 0
 points_1 = 0
 points_2 = 0
+total_1 = 0
+total_2 = 0
+avg_1 = 0
+avg_2 = 0
+count_1 = 0
+count_2 = 0
 
 # User input
 player_1 = input("Enter name of the first player: ")
@@ -48,12 +54,16 @@ while ((set_1 != sets) or (set_2 != sets)):
         points_scored_1 = int(input())
         if ((points_1 - points_scored_1) < 0) or ((points_1 - points_scored_1) == 1):
             c = 2
+            count_1 += 1
             print("{} has ".format(player_1) + str(points_1) + " points left.")
         else:
             c = 2
+            count_1 += 1
             points_1 -= points_scored_1
             print("{} has ".format(player_1) + str(points_1) + " points left.")
-
+        total_1 += points_scored_1
+        avg_1 = total_1/count_1
+        print("Player {}".format(player_1) + " has " + str(avg_1) + " points average.")
         # Leg count for the first player
         if (points_1 == 0):
             leg_1 += 1
@@ -74,17 +84,23 @@ while ((set_1 != sets) or (set_2 != sets)):
         if (set_1 == sets):
             print("Player {} won the game.".format(player_1))
             break
+
     if (c == 2):
         # Point count for the second player
         print("How many points player {} scored: ".format(player_2))
         points_scored_2 = int(input())
         if ((points_2 - points_scored_2) < 0) or ((points_2 - points_scored_2) == 1):
             c = 1
+            count_2 += 1
             print("{} has ".format(player_2) + str(points_2) + " points left.")
         else:
             c = 1
+            count_2 += 1
             points_2 -= points_scored_2
             print("{} has ".format(player_2) + str(points_2) + " points left.")
+        total_2 += points_scored_2
+        avg_2 = total_2/count_2
+        print("Player {}".format(player_2) + " has " + str(avg_2) + " points average.")
 
         # Leg count for the second player
         if (points_2 == 0):
