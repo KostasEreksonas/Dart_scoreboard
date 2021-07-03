ed63934cbae7c74dee3188de436a8be64475554b
KostasEreksonas <k.ereksonas@gmail.com>
2021-07-03 22:44:23 +0300

    Created a placeholder file for creating a dart score calculator program's graphical user interface

---
+++ Dart_score_calculator_GUI.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Filename Dart_score_calculator_GUI.py
+
+__version__ = '0.1'
+__author__ = 'Kostas Ereksonas'
+
+import sys
+
+# Import needed PyQt5 widgets for creating a dart score calculator program's graphical user interface
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import Q(...)
