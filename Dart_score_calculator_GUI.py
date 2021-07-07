e49116fd7b45e71c20f650d0ab1d293d68dc8941
KostasEreksonas <k.ereksonas@gmail.com>
2021-07-07 19:15:00 +0300

    Created a main window class in GUI version of the program

--- Dart_score_calculator_GUI.py
+++ Dart_score_calculator_GUI.py
@@ -9,6 +9,12 @@ import sys
 
 # Import needed PyQt5 widgets for creating a dart score calculator program's graphical user interface
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QGridLayout
 from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import Q(...)
+
+class scoreboardUI(QMainWindow):
+    """Main window of dart scoreboard."""
+    def __init__(self):
+        super().__init__()
