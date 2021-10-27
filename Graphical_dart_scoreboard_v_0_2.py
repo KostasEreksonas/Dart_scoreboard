819b1c37daff51038b59a27d8000252fece1e7e0
KostasEreksonas <k.ereksonas@gmail.com>
2021-10-27 20:00:21 +0300

    Added version, author and dependency information

--- Graphical_dart_scoreboard_v_0_2.py
+++ Graphical_dart_scoreboard_v_0_2.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 
+# Filename: Graphical_dart_scoreboard_v_0_2.py
 
+# Dependencies: Python3, PyQt5.QtCore, PyQt5.QtWidgets
+
+__version__ = "0.2"
+__author__ = "Kostas Ereksonas"
